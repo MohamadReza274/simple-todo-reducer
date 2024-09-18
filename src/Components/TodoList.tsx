f65519b80452855356ed9f817fbd44e6756a39aa
MohamadReza <reza.mohamadi.98115@gmail.com>
2024-09-18 21:29:30 +0430

    Refactoring: global state

--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -1,22 +1,19 @@
 import {Todo} from "../Reducers/useTodoReducer.ts";
 import TodoContext from "../TodoContext.ts";
+import {useContext} from "react";
 
 const TodoList = () => {
+    const {todos} = useContext(TodoContext);
     return (
-        <TodoContext.Consumer>
-            {({todos}) => {
-                return <ul role="list" className="space-y-3">
-                    {
-                        todos.map((todo: Todo) => (
-                            <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
-                                <h2>{todo.title}</h2>
-                                <p>{todo.description}</p>
-
-                            </li>))
-                    }
-                </ul>
-            }}
-        </TodoContext.Consumer>
+        <ul role="list" className="space-y-3">
+            {
+                todos.map((todo: Todo) => (
+                    <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                        <h2>{todo.title}</h2>
+                        <p>{todo.description}</p>
+                    </li>))
+            }
+        </ul>
     );
 };
 
