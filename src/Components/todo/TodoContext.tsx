07b2cfed82f42d4426d002f2ce2724bb9ab0986e
MohamadReza <reza.mohamadi.98115@gmail.com>
2024-10-07 14:25:22 +0430

    add delete functionality for todos.

--- src/Components/todo/TodoContext.tsx
+++ src/Components/todo/TodoContext.tsx
@@ -1,28 +1,27 @@
-import React, {createContext, Dispatch, useReducer} from "react";
-import useTodoReducer, {Todo, TodoAction} from "./useTodoReducer.ts";
-import {todoList} from './data.json'
-
+import React, { createContext, Dispatch, useReducer } from "react";
+import useTodoReducer, { Todo, TodoAction } from "./useTodoReducer.ts";
+import { todoList } from "./data.json";
 
 interface ContextType {
-    todos: Todo[];
-    dispatch: Dispatch<TodoAction>;
+  todos: Todo[];
+  dispatch: Dispatch<TodoAction>;
 }
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-export const TodoContextProvider = createContext<ContextType>({} as ContextType);
+export const TodoContextProvider = createContext<ContextType>(
+  {} as ContextType
+);
 
-const TodoContext = ({children}: Props) => {
-    const [todos, dispatch] = useReducer(useTodoReducer, todoList);
-
-
-    return (
-        <TodoContextProvider.Provider value={{todos, dispatch}}>
-            {children}
-        </TodoContextProvider.Provider>
-    )
-}
+const TodoContext = ({ children }: Props) => {
+  const [todos, dispatch] = useReducer(useTodoReducer, todoList);
+  return (
+    <TodoContextProvider.Provider value={{ todos, dispatch }}>
+      {children}
+    </TodoContextProvider.Provider>
+  );
+};
 
 export default TodoContext;
--- src/Components/todo/TodoList.tsx
+++ src/Components/todo/TodoList.tsx
@@ -1,19 +1,34 @@
-import {Todo} from "./useTodoReducer.ts";
-import {useTodos} from "./index.ts";
+import { Todo } from "./useTodoReducer.ts";
+import { useTodos } from "./index.ts";
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
 
 const TodoList = () => {
-    const {todos} = useTodos();
-    return (
-        <ul role="list" className="space-y-3">
-            {
-                todos.map((todo: Todo) => (
-                    <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
-                        <h2>{todo.title}</h2>
-                        <p>{todo.description}</p>
-                    </li>))
-            }
-        </ul>
-    );
+  const { todos, dispatch } = useTodos();
+  if (todos.length == 0) {
+    return <p className="text-xl text-gray-400 pl-4">There is no todo.</p>;
+  }
+  return (
+    <ul role="list" className="space-y-3">
+      {todos.map((todo: Todo) => (
+        <li className="flex items-center justify-between overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+          <div>
+            <h2>{todo.title}</h2>
+            <p>{todo.description}</p>
+          </div>
+          <div className="flex flex-col gap-y-4">
+            <button>
+              <PencilSquareIcon className="w-4 h-4 text-blue-500" />
+            </button>
+            <button
+              onClick={() => dispatch({ type: "REMOVE", todoId: todo.id })}
+            >
+              <TrashIcon className="w-4 h-4 text-red-500" />
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default TodoList;
